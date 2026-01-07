c9fbea4daaa84ec12c0834adac7c75b36defba24
Huilian Patrik Seidel <huilianps@outlook.com>
2026-01-07 01:58:28 -0300

    refactor: remove demo components and scripts

--- src/components/demos/index.ts
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright (c) 2026 GoodMultitracks contributors
-// Demo Components
-export { BravuraDemo } from './BravuraDemo';
-export { ChordAnalysisDemo } from './ChordAnalysisDemo';
-
